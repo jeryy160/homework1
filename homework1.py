1c9973807485016169aed403ba9958278a25e1bc
jery <jery33@gmail.com>
2025-02-22 11:57:42 +0100

    Mssage de validation

---
+++ homework1.py
@@ -0,0 +1,42 @@
+#string
+name= "alice"
+print(name)
+
+#integer
+age= 25
+print(age)
+
+#float
+height= 5.6
+print(height)
+
+#boolean
+is_student= "true"
+print(is_student)
+
+name= "alice" #sting
+age=25 #integer
+height=5.6 #float
+is_student="true" #boolean
+
+print("NAME:",name)
+print("AGE:",age)
+print("HEIGHT",height)
+print("IS_STUDENT",is_student)
+
+
+name = "jhon"  #string
+age = 32  #integer
+city = "los angelos" #string
+is_student = "true"  #boalen
+tall = 1.8  #float  
+
+  #THE PRINTS
+  
+print("NAME:",name) 
+print("AGE:",age)
+print("CITY:",city)
+print("THE STUDENT IS:",is_student)
+print("TALL:",tall,"m")
+
+
